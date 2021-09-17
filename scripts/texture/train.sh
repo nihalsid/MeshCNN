f3cfc3ff601521a50955e7df060096a6dd57eb1f
nihalsid <yawarnihal@gmail.com>
2021-09-17 18:06:23 +0200

    add slurm script

--- scripts/texture/train.sh
+++ scripts/texture/train.sh
@@ -1,4 +1,18 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+#SBATCH --job-name graphnn
+##SBATCH --nodes=1
+##SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64gb
+#SBATCH --gpus=1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=yawar.siddiqui@tum.de
+##SBATCH --exclude=char,pegasus,tarsonis,gondor,moria,seti,sorona,umoja,lothlann
+#SBATCH --exclude=char,pegasus,tarsonis,balrog,daidalos,gimli,himring,hithlum
+#SBATCH --partition=debug
+#SBATCH --qos=normal
 
 ## run the training
-python train.py --dataroot datasets/cube_tex/ --name texture_test --arch meshunet --dataset_mode texture --ncf 32 64 128 256 --ninput_edges 7440 --pool_res 5580 3720 1860 --resblocks 3 --batch_size 2 --lr 0.001 --num_aug 0 --use_single_view --num_threads 0 --save_epoch_freq 50  --save_latest_freq 500 --run_test_freq 50 --continue_train
+cd /rhome/ysiddiqui/MeshCNN
+python train.py --dataroot datasets/cube_tex/ --name texture_test --arch meshunet --dataset_mode texture --ncf 32 64 128 256 --ninput_edges 7440 --pool_res 5580 3720 1860 --resblocks 3 --batch_size 4 --lr 0.0002 --num_aug 0 --num_threads 8 --save_epoch_freq 1  --save_latest_freq 1500 --run_test_freq 1
